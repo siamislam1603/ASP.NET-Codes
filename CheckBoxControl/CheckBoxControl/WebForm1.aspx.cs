10e66ac713581252b95e1529dcdc989966f2db71
siamislam1603 <30402883+siamislam1603@users.noreply.github.com>
2020-01-04 23:26:28 +0600

    CheckBox control added

---
+++ CheckBoxControl/CheckBoxControl/WebForm1.aspx.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Text;
+
+namespace CheckBoxControl
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                GraduateCheckBox.Checked = true;
+                PostGraduateCheckBox.Focus();
+            }
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            StringBuilder str = new StringBuilder();
+            if (GraduateCheckBox.Checked)
+            {
+                str.Append(" "+"Graduate");
+            }
+            if (PostGraduateCheckBox.Checked)
+            {
+                str.Append(" " + "Postgraduate");
+            }
+            if (DoctorateCheckBox.Checked)
+            {
+                str.Append(" " + "Doctorate");
+            }
+            Response.Write("Your selected education is" + str +"<br/>");
+        }
+    }
+}
