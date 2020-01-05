4fb646955a475c475b48610e84e24fabb4b2acbf
siamislam1603 <30402883+siamislam1603@users.noreply.github.com>
2020-01-05 23:14:14 +0600

    Data retreived from db to dropDownList

---
+++ DropDownList/DropDownList/DataFromDb.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace DropDownList
+{
+    public partial class DataFromDb : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                string cs = ConfigurationManager.ConnectionStrings["DBMS"].ConnectionString;
+                using (SqlConnection con = new SqlConnection(cs))
+                {
+                    SqlCommand cmd = new SqlCommand("Select CityId,CityName,Country from City", con);
+                    con.Open();
+                    DropDownList1.DataSource = cmd.ExecuteReader();
+                    DropDownList1.DataTextField = "CityName";
+                    DropDownList1.DataValueField = "CityId";
+                    DropDownList1.DataBind();
+                }
+            }
+        }
+    }
+}
