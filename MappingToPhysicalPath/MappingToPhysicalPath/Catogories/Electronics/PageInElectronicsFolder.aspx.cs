84722c77c583d4c47ef0e51b7c61eb667aea0191
siamislam1603 <30402883+siamislam1603@users.noreply.github.com>
2020-01-18 19:49:33 +0600

    Mapping to physical path from virtual path

---
+++ MappingToPhysicalPath/MappingToPhysicalPath/Catogories/Electronics/PageInElectronicsFolder.aspx.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace MappingToPhysicalPath.Catogories.Electronics
+{
+    public partial class PageInElectronicsFolder : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Response.Write("Page Currently Executing Path:<br/>" + Server.MapPath(".") + "<br/><br/>");
+            Response.Write("Page Currently Executing it's Parent Path:<br/>" + Server.MapPath("..") + "<br/><br/>");
+            Response.Write("Page Currently Executing it's Root Path:<br/>" + Server.MapPath("~") + "<br/><br/>");
+            Response.Write("Page Currently Executing it's Root Path:<br/>" + Server.MapPath("../..") + "<br/><br/>");
+        }
+    }
+}
