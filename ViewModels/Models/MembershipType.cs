7e777b786700e1251ce7856ad1c1d02d63bf0e75
siamislam1603 <30402883+siamislam1603@users.noreply.github.com>
2020-03-20 21:27:51 +0600

    Working with Data,(Solution of ex-1) Added New Column to table and update the table

--- ViewModels/Models/MembershipType.cs
+++ ViewModels/Models/MembershipType.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,6 +9,8 @@ namespace ViewModels.Models
     public class MembershipType
     {
         public byte id { get; set; }
+        [Required]
+        public string name { get; set; }
         public short signUpFee { get; set; }
         public short durationInMonths { get; set; }
         public short discountRate { get; set; }
