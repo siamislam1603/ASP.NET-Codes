713cbd4627a9157d5a5ed96c980ab9f747d9fea1
siamislam1603 <30402883+siamislam1603@users.noreply.github.com>
2020-04-03 21:59:22 +0600

    Building RESTful services,Building an api

---
+++ ViewModels/Controllers/Api/CustomersController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using ViewModels.Models;
+
+namespace ViewModels.Controllers.Api
+{
+    public class CustomersController : ApiController
+    {
+        private MyDBContext context;
+        public CustomersController()
+        {
+            context = new MyDBContext();
+        }
+        //GET /api/customers
+        public IEnumerable<Customer> GetCustomers()
+        {
+            return context.Customers.ToList();
+        }
+
+        //GET /api/Customer/1
+        public Customer GetCustomer(int id)
+        {
+            var customer = context.Customers.SingleOrDefault(c => c.id == id);
+            if (customer == null)
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            return customer;
+        }
+        //POST /api/customers
+        [HttpPost]
+        public Customer CreateCustomer(Customer customer)
+        {
+            if (!ModelState.IsValid)
+            {
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+            }
+            context.Customers.Add(customer);
+            context.SaveChanges();
+            return customer;
+        }
+        //PUT /api/customer/1
+        [HttpPut]
+        public void UpdateCustomer(int id,Customer customer)
+        {
+            if (!ModelState.IsValid)
+            {
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+            }
+            var customerInDb = context.Customers.SingleOrDefault(c => c.id == id);
+            if (customerInDb == null)
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            customerInDb.name = customer.name;
+            customerInDb.IsSubscribedToNewsLetter = customer.IsSubscribedToNewsLetter;
+            customerInDb.membershipTypeId = customer.membershipTypeId;
+            customerInDb.birthdate = customer.birthdate;
+            context.SaveChanges();
+        }
+        //DELETE /api/customer/1
+        [HttpDelete]
+        public void deleteCustomer(int id)
+        {
+            var customer = context.Customers.SingleOrDefault(c => c.id == id);
+            if (customer == null)
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            context.Customers.Remove(customer);
+            context.SaveChanges();
+        }
+    }
+}
--- ViewModels/Global.asax.cs
+++ ViewModels/Global.asax.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using ViewModels.App_Start;
 
 namespace ViewModels
 {
@@ -12,6 +14,7 @@ namespace ViewModels
     {
         protected void Application_Start()
         {
+            GlobalConfiguration.Configure(WebApiConfig.Register);
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
