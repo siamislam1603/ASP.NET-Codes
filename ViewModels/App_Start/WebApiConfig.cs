009cbbc085f3003e6ab859f73fccf46229d68f39
siamislam1603 <30402883+siamislam1603@users.noreply.github.com>
2020-04-04 21:40:43 +0600

    Building RESTful services,Using camel notation to webApiConfig

--- ViewModels/App_Start/WebApiConfig.cs
+++ ViewModels/App_Start/WebApiConfig.cs
@@ -3,13 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Http;
-
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 namespace ViewModels.App_Start
 {
     public class WebApiConfig
     {
         public static void Register(HttpConfiguration config)
         {
+            var settings = config.Formatters.JsonFormatter.SerializerSettings;
+            settings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+            settings.Formatting = Formatting.Indented;
             config.MapHttpAttributeRoutes();
 
             config.Routes.MapHttpRoute(
